93c33f9e0ca09332775989a48df8e4a29189112c
Oreki <88679208+Orekim@users.noreply.github.com>
2021-09-15 17:25:53 +0800

    Add files via upload
    My first code

---
+++ First.py
@@ -0,0 +1,17 @@
+x = int(input("Enter numerator #1: "))
+y = int(input("Enter denomirator #1: "))
+a = int(input("Enter numerator #2: "))
+b = int(input("Enter denomirator #2: "))
+
+A = x * b
+B = y * b
+C = a * y
+D = b * y
+
+Nume = A + C
+sum = (A / B) + (C/D)
+
+print(f"{x} / {y} + {a} / {b} = {Nume} / {B}")
+
+
+
