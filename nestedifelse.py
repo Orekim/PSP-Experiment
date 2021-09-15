04c7bc47486f5e0ea8551478d9ebdb8fe152e886
Oreki <88679208+Orekim@users.noreply.github.com>
2021-09-15 17:25:03 +0800

    Add files via upload

---
+++ nestedifelse.py
@@ -0,0 +1,17 @@
+x = float(input("Enter marks:   "))
+
+if x >100.0 or x <0.0:
+    print("invalid marks")
+else:
+    if x>=80.0:
+        print("A")
+
+    if x>=60.0:
+        print("b")
+
+    if x>=50.0:
+        print("c")
+
+    if x<=50.0:
+         print("f")
+ 
