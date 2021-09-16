a54615752643d6494cb54f7f2a84471a28347978
Oreki <88679208+Orekim@users.noreply.github.com>
2021-09-16 20:14:50 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+while True:
+    print("Who are you?")
+    name = input()
+    if name != "Aiman":
+        continue
+    print("Hello, Aiman. Do you know the password?")
+    password = input()
+    if password == "yothispasswordsuck":
+        break
+print("Access granted.")
+
+
+       
+         
+
